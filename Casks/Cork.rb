f9d8c0d2d397b4cea13b288f4957796756d9b243
Enigmatic DB <modelar_zalozky0q@icloud.com>
2023-03-06 23:37:44 +0100

    Doesn't work

--- Casks/Cork.rb
+++ Casks/Cork.rb
@@ -1,4 +1,4 @@
-cask "Cork" do
+cask "cork" do
     version "0.1.1"
     sha256 "196a979ad948f15464a40a802b03e8b0dbeb9c27da72383b986499bad29abac1"
   
@@ -11,4 +11,4 @@ cask "Cork" do
     depends_on macos: ">= :ventura"
   
     app "Cork.app"
-  end
+end
