e8b2c797db83b3375c1d4387d26116ad273a9cf3
Enigmatic DB <modelar_zalozky0q@icloud.com>
2023-03-06 23:26:33 +0100

    Added first definition

---
+++ Casks/Cork.rb
@@ -0,0 +1,14 @@
+cask "Cork" do
+    version "0.1.1"
+    sha256 "196a979ad948f15464a40a802b03e8b0dbeb9c27da72383b986499bad29abac1"
+  
+    url "https://corkmac.app/RLS/#{version}.app"
+    name "Cork"
+    desc "The Homebrew GUI"
+    homepage "https://www.corkmac.app"
+  
+    auto_updates true
+    depends_on macos: ">= :ventura"
+  
+    app "Cork.app"
+  end
