c7adedce8bbca42ae4f8db58756aa6025abbb352
Enigmatic DB <modelar_zalozky0q@icloud.com>
2023-03-07 17:29:32 +0100

    Disabled SHA

--- Casks/Cork.rb
+++ Casks/Cork.rb
@@ -1,6 +1,6 @@
 cask "cork" do
     version "0.1.2"
-    sha256 "281a2fbd3a5c1ce96903b61e7ec0783d28bbcc771831956835ab72c1fab1bea9"
+    sha256 :no_check
   
     url "https://corkmac.app/RLS/#{version}/Cork.app"
     name "Cork"
