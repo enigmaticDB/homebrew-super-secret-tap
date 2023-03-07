355ac498bc9173e86ec4dc7c8c949bf5fb5a7370
Enigmatic DB <modelar_zalozky0q@icloud.com>
2023-03-07 17:46:53 +0100

    Zipped up

--- Casks/Cork.rb
+++ Casks/Cork.rb
@@ -2,7 +2,7 @@ cask "cork" do
     version "0.1.2"
     sha256 "281a2fbd3a5c1ce96903b61e7ec0783d28bbcc771831956835ab72c1fab1bea9"
   
-    url "https://corkmac.app/RLS/#{version}/Cork.app"
+    url "https://corkmac.app/RLS/#{version}/Cork.zip"
     name "Cork"
     desc "The Homebrew GUI"
     homepage "https://www.corkmac.app"
