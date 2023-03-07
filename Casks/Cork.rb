669afed3eed50af9aad0020b38ff586d950c2b71
Enigmatic DB <modelar_zalozky0q@icloud.com>
2023-03-07 12:39:13 +0100

    First zip attempt

--- Casks/Cork.rb
+++ Casks/Cork.rb
@@ -2,15 +2,15 @@ cask "cork" do
     version "0.1.2"
     sha256 :no_check
   
-    url "https://corkmac.app/RLS/#{version}/Cork.app"
+    url "https://corkmac.app/RLS/#{version}/Cork.zip"
     name "Cork"
     desc "The Homebrew GUI"
-    homepage "https://www.corkmac.app"
+    homepage "https://www.corkmac.app/"
   
     auto_updates true
     depends_on macos: ">= :ventura"
   
-    app "Cork.app"
+    app "Cork/Cork.app"
 
     uninstall quit: "com.davidbures.Cork"
 end
