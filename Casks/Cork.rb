df29bd20d618bc936871f7b00101f740fe0ac312
Enigmatic DB <modelar_zalozky0q@icloud.com>
2023-09-05 00:17:11 +0200

    Version Bump

--- Casks/Cork.rb
+++ Casks/Cork.rb
@@ -1,10 +1,10 @@
 cask "cork" do
-    version "1.1"
+    version "1.2"
     sha256 :no_check
   
     url "https://corkmac.app/RLS/#{version}/Cork.zip"
     name "Cork"
-    desc "The Homebrew GUI"
+    desc "The Homebrew Companion"
     homepage "https://www.corkmac.app"
   
     auto_updates false
