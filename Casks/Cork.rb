2220f9c78e821701d7288c14dc0370fe52058991
David Bureš <buresdv@gmail.com>
2024-06-01 23:37:31 +0200

    Version Bump

--- Casks/Cork.rb
+++ Casks/Cork.rb
@@ -1,10 +1,10 @@
 cask "cork" do
-    version "1.4"
+    version "1.4.1"
     sha256 :no_check
   
     url "https://corkmac.app/RLS/#{version}/Cork.zip"
     name "Cork"
-    desc "The Homebrew Companion"
+    desc "The Ultimate Homebrew Companion"
     homepage "https://www.corkmac.app"
   
     auto_updates false
